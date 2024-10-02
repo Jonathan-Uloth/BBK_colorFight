d107ce4b5b4835f9f9b478b55bb1148138eda45c
Jonathan <jonathan.uloth@gmail.com>
2024-10-02 14:09:46 +0200

    Feldgenerierung in GameEngine

--- gameengine.cpp
+++ gameengine.cpp
@@ -1,5 +1,17 @@
 #include "gameengine.h"
+#include <QRandomGenerator>
 
 GameEngine::GameEngine(QObject *parent)
     : QObject{parent}
-{}
+{
+    // Dimensionieren verschachtelte Liste auf 13x17
+    cells = QList<QList<Colors>>(13, QList<Colors>(17));
+
+    // Randomly fill each cell with a random color
+    for (int row = 0; row < cells.size(); ++row) {
+        for (int col = 0; col < cells[row].size(); ++col) {
+            cells[row][col] = static_cast<Colors>(QRandomGenerator::global()->bounded(0,COLORCOUNT));
+        }
+    }
+}
+
--- gameengine.h
+++ gameengine.h
@@ -3,11 +3,20 @@
 
 #include <QObject>
 
+
+// Farbenkonfiguration
+const int COLORCOUNT = 6; // Anzahl Farben
+enum Colors { RED, BLUE, GREEN, YELLOW, MAGENTA, ORANGE };
+
+
 class GameEngine : public QObject
 {
     Q_OBJECT
 public:
     explicit GameEngine(QObject *parent = nullptr);
+
+private:
+    QList< QList<Colors> > cells;
 
 signals:
 };
